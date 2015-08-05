f5acc8b7aaf81165a929758f94ac11c4375858ab
jonny166 <kathy.church@gmail.com>
2015-08-04 20:51:03 -0400

    Update package.json and gulpfile to install and host site on webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // Node modules
 var fs = require('fs'), vm = require('vm'), merge = require('deeply'), chalk = require('chalk'), es = require('event-stream');
+var plugins = require("gulp-load-plugins")({lazy:false});
 
 // Gulp and plugins
 var gulp = require('gulp'), rjs = require('gulp-requirejs-bundler'), concat = require('gulp-concat'), clean = require('gulp-clean'),
@@ -63,7 +64,13 @@ gulp.task('clean', function() {
         .pipe(clean());
 });
 
-gulp.task('default', ['html', 'js', 'css'], function(callback) {
+gulp.task('connect', plugins.connect.server({
+    root: ['dist'],
+    port: 8080,
+    livereload: true
+}));
+
+gulp.task('default', ['connect', 'html', 'js', 'css'], function(callback) {
     callback();
     console.log('\nPlaced optimized files in ' + chalk.magenta('dist/\n'));
 });
